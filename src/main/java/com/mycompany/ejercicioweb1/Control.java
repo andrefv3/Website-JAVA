c0a5552d5fb3740bda788b664b7f3f94f5b2db36
Andrés <52301418+andres345@users.noreply.github.com>
2022-09-22 22:20:15 -0500

    primer commit

---
+++ src/main/java/com/mycompany/ejercicioweb1/Control.java
@@ -0,0 +1,113 @@
+package com.mycompany.ejercicioweb1;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author juan esteban
+ */
+@WebServlet(name = "Control", urlPatterns = {"/Control"})
+public class Control extends HttpServlet {
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try ( PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet control</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet control at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+     protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        //processRequest(request, response);
+        response.setContentType("text/html;charset=UTF-8");
+
+        String n = request.getParameter("txt_nombres");
+        String a = request.getParameter("txt_apellidos");
+
+        try ( PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>\n" +
+"<html>\n" +
+"    <head>\n" +
+"        <meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\">\n" +
+"        <link rel=\"stylesheet\" href=\"https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css\" \n" +
+"            integrity=\"sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm\" crossorigin=\"anonymous\">\n" +
+"        <link rel=\"stylesheet\" href=\"css/estilos.css\">\n" +
+"        <link rel=\"icon\" href=\"img/favicon.ico\";>\n" +
+"        <title>Servlet control</title>\n" +
+"    </head>\n" +
+"    <body>\n" +
+"        <div id=\"main\">\n" +
+"            <header id=\"headerP\">\n" +
+"                <a style=\"text-decoration: none;\" href=\"./\"><h1>Página de Andrés</h1></a>\n" +
+"            </header>\n" +
+"            <div class=\"card-container\">\n" +
+"                <h3><center>DOMICILIO</center></h3>        \n" +
+"                <hr>\n" + 
+"                <h2>Nombres:  " +n+"</h2>\n" +
+"                <p></p>\n" +
+"            <h2>Apellidos:  " + a +"</h2>\n"+ 
+"            </div>\n" +
+"        \n" +
+"            <div class=\"footer\">\n" +
+"                <span class=\"spanf\">Andrés Felipe Vega — 20201578113</span>\n" +
+"            </div>\n" +
+"        </div>\n" +
+"        \n" +
+"    </body>\n" +
+"</html>");
+            
+        }
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,57 @@
+<%-- 
+    Document   : index
+    Created on : 22 sept 2022, 13:14:08
+    Author     : Andres
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" 
+            integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+        <link rel="stylesheet" href="css/estilos.css">
+        <link rel="icon" href="img/favicon.ico";>
+        <title>Principal</title>
+    </head>
+    <body>
+        <div id="main">
+            <header id="headerP">
+                <a style="text-decoration: none;" href="./"><h1>Página de Andrés</h1></a>
+            </header>
+            <div class="card-container">
+                <h3><center>FORM</center></h3>        
+                <form action="Control" method="get" id="formul">
+                    <div class="form-group">
+                        <label for="exampleInputEmail1">Nombres:</label>
+                        <input type="text" class="form-control" name="txt_nombres" required/>
+                    </div>
+                    <div class="form-group">
+                        <label for="exampleInputEmail1">Apellidos:</label>
+                        <input class="form-control" type="text" name="txt_apellidos" required/>
+                    </div>
+                    <div class="boton">
+                        <input class="btn btn-primary" id="btnEnvio" type="submit" name="btn_registrar" value="Registrar"/>
+                    </div>
+                </form> 
+                <hr>
+                <center>
+                    <a href="Control">verificar servlet objeto response</a>
+                </center>
+            </div>
+        
+            <div class="footer">
+                <span class="spanf">Andrés Felipe Vega — 20201578113</span>
+            </div>
+        </div>
+        
+        <script>
+            $(document).ready(function() {
+                $('#btnEnvio').click(function() {
+                    $('input[type="text"]').val('');
+                });
+            });
+        </script>
+    </body>
+</html>
