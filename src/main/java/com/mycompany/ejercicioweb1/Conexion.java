fda1950a73758a091f8972ef2a3745ea8a4dc787
andres345 <andresfelipevega567@gmail.com>
2022-10-27 09:49:48 -0500

    ConexionBD - HEROKU

--- src/main/java/com/mycompany/ejercicioweb1/Conexion.java
+++ src/main/java/com/mycompany/ejercicioweb1/Conexion.java
@@ -25,9 +25,9 @@ public class Conexion {
 
     public Conexion() {
         this.conectar = null;
-        this.BD = "bd_vega";
-        this.usuario = "postgres";
-        this.contra = "123456";
+        this.BD = "jdbc:postgresql://ec2-3-225-110-188.compute-1.amazonaws.com:5432/decco43vde7cpm";
+        this.usuario = "qcwmjonhdhvcgf";
+        this.contra = "7ddbb41cf60fd05b075880d08f13907e5e7861e15191ee30565b6289918a52b3";
     }
     
     public synchronized static Conexion getConexion() {
@@ -40,9 +40,9 @@ public class Conexion {
     public void conectar(){
         try{
             Class.forName("org.postgresql.Driver");
-            this.BD = "bd_vega";
-            this.usuario = "postgres";
-            this.contra = "123456";
+            this.BD = "jdbc:postgresql://ec2-3-225-110-188.compute-1.amazonaws.com:5432/decco43vde7cpm";
+            this.usuario = "qcwmjonhdhvcgf";
+            this.contra = "7ddbb41cf60fd05b075880d08f13907e5e7861e15191ee30565b6289918a52b3";
             this.conectar = (Connection) DriverManager.getConnection(BD, usuario, contra);
         }catch(ClassNotFoundException | HeadlessException | SQLException e ){
             System.out.println("Error al conectar: " + e);
